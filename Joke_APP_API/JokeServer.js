badad42dba26a2a2d48287059d25e573d043ef93
Hamidkn <hamid.keshmiri@gmail.com>
2022-02-03 22:35:42 +0100

    checkboxs are active

--- Joke_APP_API/JokeServer.js
+++ Joke_APP_API/JokeServer.js
@@ -16,7 +16,7 @@ const url = "https://v2.jokeapi.dev/joke/"
 
 app.get("/", function(req, res) {
     // res.sendFile(__dirname + "/views/index.ejs");
-    res.render("index", { Joke: null, error: null });
+    res.render("index", { joke: null, error: null });
 });
 
 app.post("/", function(req, res) {
@@ -28,23 +28,67 @@ app.post("/", function(req, res) {
     let racist = req.body.racist;
     let sexist = req.body.sexist;
     let explicit = req.body.explicit;
-    console.log(category);
+    // console.log(religious)
+    // exports.myHandler = async(req, res) => {
+    //     const {
+    //         nsfw,
+    //         religious,
+    //         political,
+    //         racist,
+    //         sexist,
+    //         explicit
+    //     } = req.body
+    // }
+
+    exports.myHandler = async(req, res) => {
+            const { flags } = req.body
+            flags.forEach(flag => console.log(flag))
+        }
+        // console.log(flags);
+
+    // console.log(nsfw);
     let url = `https://v2.jokeapi.dev/joke/${category}`;
 
-    request(url, function(err, res, body) {
+    request(url, function(err, response, body) {
         if (err) {
-            res.render("index", { Joke: null, error: "Incorrect category!" });
+            res.render("index", { joke: null, jokedelivery: null, error: "Incorrect category!" });
         } else {
             let data = JSON.parse(body);
-            if (category == undefined) {
-                res.render('index', { Joke: null, error: 'Error, category is invalid!' });
-            } else {
-                if (type == 'single' || type == 'twopart') {
-
-                }
-                let JokeText = `It's ${data.main.temp} degrees in ${data.name}, ${data.sys.country}!`;
-                res.render('index', { weather: weatherText, error: null });
+            if (nsfw == 'on') {
+                data.flags.nsfw = true;
             }
+            if (religious == 'on') {
+                data.flags.religious = true;
+            }
+            if (political == 'on') {
+                data.flags.political = true;
+            }
+            if (racist == 'on') {
+                data.flag.racist = true;
+            }
+            if (sexist == 'on') {
+                data.flags.sexist = true;
+            }
+            if (explicit == 'on') {
+                data.flags.explicit = true;
+            }
+            console.log(data);
+            if (category == undefined) {
+                res.render('index', { joke: null, jokedelivery: null, error: 'Error, category is invalid!' });
+            }
+            if (type == 'single') {
+
+                let JokeText = data.joke;
+                res.render('index', { joke: JokeText, jokedelivery: null, error: null });
+            } else if (type == 'twopart') {
+                let JokeText = data.setup;
+                let JokeDelivery = data.delivery;
+                res.render('index', { joke: JokeText, jokedelivery: JokeDelivery, error: null });
+            }
+            // let JokeText = data.joke;
+            // // console.log(data);
+            // // let JokeText = `It's ${data.main.temp} degrees in ${data.name}, ${data.sys.country}!`;
+            // res.render('index', { joke: JokeText, error: null });
         }
     });
 })
