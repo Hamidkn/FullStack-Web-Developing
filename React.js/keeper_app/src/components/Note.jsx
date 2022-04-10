d60064c51bfcbcf334d9f4365c1ddb98d6d38102
Hamidkn <hamid.keshmiri@gmail.com>
2022-04-10 23:30:03 +0200

    Keeper_app part 1

---
+++ React.js/keeper_app/src/components/Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Note(){
+    return (
+        
+        <div className="note">
+            <h1>first Note.</h1>
+            <p>This is the first note!</p>
+        </div>
+    );
+};
+
+export default Note;
